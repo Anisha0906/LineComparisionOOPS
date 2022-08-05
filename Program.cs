40e4c3fcca6ea222f0baeef81f7079d59f0ec944
Anisha0906 <anishadas880@gmail.com>
2022-08-05 21:58:43 +0530

    Add:Check Two Lines Are Equal or Not

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace LineComparisonComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("... Welcome to Line Comparison Computation ... ");
+            LineComparision LineCom = new LineComparision();
+            LineCom.lengthOFLine();
+            LineCom.functioncheckequallines();
+            Console.ReadKey();
+        }
+    }
+
+}
